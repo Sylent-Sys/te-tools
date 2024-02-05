62bd84e405664aed8d0d7a77141f149d80c91077
Sylent-Sys <renaldiapriyanto419@gmail.com>
2024-02-05 19:17:43 +0700

    feat: Remove jest.config.mjs file

--- source/jest.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('jest').Config} */
-const config = {
-  clearMocks: true,
-  transform: {},
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/src/'],
-};
-
-export default config;
